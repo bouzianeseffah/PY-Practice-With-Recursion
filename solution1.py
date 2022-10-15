af7a3e194f6d94a5f19fe9a2ecbf7c4eefbaa73d
bouzianes <fcbdreams1991@gmail.com>
2022-10-15 12:16:15 -0400

    left the bonus

--- solution1.py
+++ solution1.py
@@ -1,1 +1,8 @@
 # Write code for algorithm 1 below
+def positive(num):
+    if num == 0:
+        return "done"
+    else:
+        return positive( num - 1)    
+
+print(positive(4))
--- solution2.py
+++ solution2.py
@@ -1,1 +1,9 @@
 # Write code for algorithm 2 below
+def nutural_number(n, i = 1):
+    if i  > n:
+        return "done"
+    else:
+        print(i)  
+        nutural_number(n, i + 1)  
+
+nutural_number(3)
